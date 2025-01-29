2d32ff0b1493ec9ad6c93043d1f5d5d4359556aa
PopSmileAx <ina12aya21@gmail.com>
2025-01-30 00:06:15 +0900

    47曲一覧対応

--- Bingo.js
+++ Bingo.js
@@ -25,7 +25,7 @@ function makeRandom(pathNo){
 // (2) 取得するファイルの設定
   switch (pathNo){
     case 47:
-      xhr.open('get', '/Data/MusicName.txt');
+      xhr.open('get', '/Data/47Music.txt');
       break;
     default:
       break;
